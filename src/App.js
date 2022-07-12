050aee6712970ebd3c11f423968ef2b9b1077c34
mcvollenweider <mcvollenweider@gmail.com>
2022-07-12 09:27:43 -0400

    uncategorized budget card

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Container from "react-bootstrap/Container";
 import { Button, Stack } from "react-bootstrap";
 import BudgetCard from "./components/BudgetCard";
+import UncategorizedBudgetCard from "./components/UncategorizedBudgetCard";
 import AddBudgetModal from "./components/AddBudgetModal";
 import AddExpenseModal from "./components/AddExpenseModal";
 import { useState } from "react";
@@ -51,6 +52,7 @@ setAddExpenseModalBudgetId(budgetId)
               />
             );
           })}
+          <UncategorizedBudgetCard/>
         </div>
       </Container>
       <AddBudgetModal
--- src/components/BudgetCard.js
+++ src/components/BudgetCard.js
@@ -16,18 +16,18 @@ if (amount > max){
           <div className="me-2">{name}</div>
           <div className="d-flex align-items-baseline">
             {currencyFormatter.format(amount)}{" "}
-            <span className="text-muted fs-6 ms-1">
+            {max && <span className="text-muted fs-6 ms-1">
               /{currencyFormatter.format(max)}
-            </span>
+            </span>}
           </div>
         </Card.Title>
-        <ProgressBar
+        { max && <ProgressBar
           className="rounded-pill"
           variant={getProgressBarVariant(amount, max)}
           min={0}
           max={max}
           now={amount}
-        />
+        />}
         <Stack direction="horizontal" gap="2" className="mt-4">
           <Button variant="outline-primary" className="ms-auto" onClick={onAddExpenseClick}>
             Add Expense
